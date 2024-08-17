c9fb37983837c37b2036b552e471618d988ca0d1
Achraf Habib <142338334+AaashrafHabib@users.noreply.github.com>
2024-08-17 18:05:27 +0100

    "RAG using Groq "

--- API/app.py
+++ API/app.py
@@ -1,52 +1,62 @@
-from fastapi import FastAPI
-from langchain.prompts import ChatPromptTemplate
-from langchain.chat_models import ChatOpenAI
-from langserve import add_routes
-import uvicorn
+import streamlit as st
 import os
-from langchain_community.llms import Ollama
-from dotenv import load_dotenv
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.embeddings import OllamaEmbeddings
 
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains import create_retrieval_chain
+from langchain_community.vectorstores import FAISS
+from langchain_groq import ChatGroq
+import time
+
+from dotenv import load_dotenv
 load_dotenv()
 
-# os.environ['OPENAI_API_KEY']=os.getenv("OPENAI_API_KEY")
+## load the Groq API key
+groq_api_key=os.environ['GROQ_API_KEY']
 
-app=FastAPI(
-    title="Langchain Server",
-    version="1.0",
-    decsription="A simple API Server"
+if "vector" not in st.session_state:
+    st.session_state.embeddings=OllamaEmbeddings()
+    st.session_state.loader=WebBaseLoader("https://docs.smith.langchain.com/")
+    st.session_state.docs=st.session_state.loader.load()
 
+    st.session_state.text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200)
+    st.session_state.final_documents=st.session_state.text_splitter.split_documents(st.session_state.docs[:50])
+    st.session_state.vectors=FAISS.from_documents(st.session_state.final_documents,st.session_state.embeddings)
+
+st.title("ChatGroq Demo")
+llm=ChatGroq(groq_api_key=groq_api_key,
+             model_name="mixtral-8x7b-32768")
+
+prompt=ChatPromptTemplate.from_template(
+"""
+Answer the questions based on the provided context only.
+Please provide the most accurate response based on the question
+<context>
+{context}
+<context>
+Questions:{input}
+
+"""
 )
+document_chain = create_stuff_documents_chain(llm, prompt)
+retriever = st.session_state.vectors.as_retriever()
+retrieval_chain = create_retrieval_chain(retriever, document_chain)
 
-# add_routes(
-#     app,
-#     ChatOpenAI(),
-#     path="/openai"
-# )
-# model=ChatOpenAI()
+prompt=st.text_input("Input you prompt here")
 
-##ollama llama2
-llm=Ollama(model="llama2")
+if prompt:
+    start=time.process_time()
+    response=retrieval_chain.invoke({"input":prompt})
+    print("Response time equal to  :",time.process_time()-start)
+    st.write(response['answer'])
 
-prompt1=ChatPromptTemplate.from_template("Write me an essay about {topic} with 100 words")
-prompt2=ChatPromptTemplate.from_template("Write me an poem about {topic} for a 5 years child with 100 words")
-
-add_routes(
-    app,
-    prompt1|llm,
-    path="/essay"
-
-
-)
-
-add_routes(
-    app,
-    prompt2|llm,
-    path="/poem"
-
-
-)
-
-
-if __name__=="__main__":
-    uvicorn.run(app,host="localhost",port=8000)
+    # With a streamlit expander
+    with st.expander("Document Similarity Search"):
+        # Find the relevant chunks
+        for i, doc in enumerate(response["context"]):
+            st.write(doc.page_content)
+            st.write("-------------------------")
+    
